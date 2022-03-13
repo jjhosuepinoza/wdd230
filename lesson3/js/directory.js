ac20cb38ab21d4206d4ba53d8cbeb6f83e703c92
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2022-03-13 01:41:16 +0100

    commit

--- lesson3/js/directory.js
+++ lesson3/js/directory.js
@@ -13,17 +13,21 @@ fetch(requestURL).then(function (response) {return response.json();})
     let companyName = document.createElement('h2');
     let companyAddress = document.createElement('p');
     let companyPhone = document.createElement('p');
-    let companySite = document.createElement('u');
+    let companySite = document.createElement('a');
     let logo = document.createElement('img');
 
     companyName.textContent = `${company.name}`; 
     companyAddress.textContent = ` ${company.address}`;
     companyPhone.textContent = ` ${company.phone}`;
     companySite.textContent = ` ${company.site}`;
-   
+ 
+  
+
     logo.setAttribute('src', company.imageurl);
     logo.setAttribute('alt', `Logo of ${company.name}`);
     logo.setAttribute('loading', 'lazy');
+    companySite.setAttribute('href', company.site);
+    companySite.setAttribute('target', 'blank');
 
     card.appendChild(logo);
     card.appendChild(companyAddress);
