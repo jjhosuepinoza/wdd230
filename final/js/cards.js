41ccdc9b93becddcbfa905c7ae180bd5b0fa4518
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2022-04-07 21:18:48 +0200

    commit

--- final/js/cards.js
+++ final/js/cards.js
@@ -4,38 +4,32 @@ const cards = document.querySelector('.cards');
 fetch(requestURL).then(function (response) {return response.json();})
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const companies = jsonObject['Temples'];
-    companies.forEach(displayCompanies);
+    const temples = jsonObject['Temples'];
+    temples.forEach(displayTemples);
   });
 
-  function displayCompanies(temple) {
+  function displayTemples(temple) {
     let card = document.createElement('section');
     let templeName = document.createElement('h2');
     let templeAddress = document.createElement('p');
     let templePhone = document.createElement('p');
-    let templeEmail = document.createElement('a');
-    let templeServices = document.createElement('p');
-    let templeHistory = document.createElement('p');
     let templeSite = document.createElement('a');
-    let templePic
-     = document.createElement('img');
+    let templeLogo = document.createElement('img');
 
     templeName.textContent = `${temple.name}`; 
     templeAddress.textContent = ` ${temple.address}`;
     templePhone.textContent = ` ${temple.phone}`;
     templeSite.textContent = ` ${temple.site}`;
-   
-    templePic
-    .setAttribute('src', temple.imageurl);
-    templePic
-    .setAttribute('alt', `templePic
-     of ${temple.name}`);
-    templePic
-    .setAttribute('loading', 'lazy');
+ 
   
 
-    card.appendChild(templeName);
-    
+    templeLogo.setAttribute('src', temple.imageurl);
+    templeLogo.setAttribute('alt', `Logo of ${temple.name}`);
+    templeLogo.setAttribute('loading', 'lazy');
+    templeSite.setAttribute('href', temple.site);
+    templeSite.setAttribute('target', 'blank');
+
+    card.appendChild(templeLogo);
     card.appendChild(templeAddress);
     card.appendChild(templePhone);
     card.appendChild(templeSite);
@@ -44,6 +38,8 @@ fetch(requestURL).then(function (response) {return response.json();})
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
     
+    
+
   }
 
 
