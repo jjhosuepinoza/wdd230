314e4d05f2b1a5d41555dd851ae80306e6740cba
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2022-04-07 21:14:30 +0200

    commit

---
+++ final/js/cards.js
@@ -0,0 +1,49 @@
+const requestURL = 'https://jjhosuepinoza.github.io/wdd230/final/data/data.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL).then(function (response) {return response.json();})
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const companies = jsonObject['Temples'];
+    companies.forEach(displayCompanies);
+  });
+
+  function displayCompanies(temple) {
+    let card = document.createElement('section');
+    let templeName = document.createElement('h2');
+    let templeAddress = document.createElement('p');
+    let templePhone = document.createElement('p');
+    let templeEmail = document.createElement('a');
+    let templeServices = document.createElement('p');
+    let templeHistory = document.createElement('p');
+    let templeSite = document.createElement('a');
+    let templePic
+     = document.createElement('img');
+
+    templeName.textContent = `${temple.name}`; 
+    templeAddress.textContent = ` ${temple.address}`;
+    templePhone.textContent = ` ${temple.phone}`;
+    templeSite.textContent = ` ${temple.site}`;
+   
+    templePic
+    .setAttribute('src', temple.imageurl);
+    templePic
+    .setAttribute('alt', `templePic
+     of ${temple.name}`);
+    templePic
+    .setAttribute('loading', 'lazy');
+  
+
+    card.appendChild(templeName);
+    
+    card.appendChild(templeAddress);
+    card.appendChild(templePhone);
+    card.appendChild(templeSite);
+    
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    cards.appendChild(card);
+    
+  }
+
+
